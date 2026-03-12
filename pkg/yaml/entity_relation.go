c3aaba9707bd42a67f88343ea1991fe43db8b0b4
bnunamak <brennan@nunamaker.de>
2026-03-12 14:23:00 +0100

    feat: support attributes in model / entity relations

--- pkg/yaml/entity_relation.go
+++ pkg/yaml/entity_relation.go
@@ -3,17 +3,19 @@ package yaml
 import "github.com/kalo-build/clone"
 
 type EntityRelation struct {
-	Type    string   `yaml:"type"`
-	For     []string `yaml:"for,omitempty"`
-	Through string   `yaml:"through,omitempty"`
-	Aliased string   `yaml:"aliased,omitempty"`
+	Type       string   `yaml:"type"`
+	For        []string `yaml:"for,omitempty"`
+	Through    string   `yaml:"through,omitempty"`
+	Aliased    string   `yaml:"aliased,omitempty"`
+	Attributes []string `yaml:"attributes,omitempty"`
 }
 
 func (f EntityRelation) DeepClone() EntityRelation {
 	return EntityRelation{
-		Type:    f.Type,
-		For:     clone.Slice(f.For),
-		Through: f.Through,
-		Aliased: f.Aliased,
+		Type:       f.Type,
+		For:        clone.Slice(f.For),
+		Through:    f.Through,
+		Aliased:    f.Aliased,
+		Attributes: clone.Slice(f.Attributes),
 	}
 }
